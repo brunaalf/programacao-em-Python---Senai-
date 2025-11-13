4d32bac306b6d293a3795205ea2e44db68ea91db
Bruna Fernandes <214554934+brunaalf@users.noreply.github.com>
2025-11-12 21:41:33 -0300

    Add files via upload

---
+++ Curso de Python - Senai/Aula_14/main.py
@@ -0,0 +1,23 @@
+#Atividade 1:
+
+import meu_modulo 
+#print('aleatorio:',meu_modulo.aleatorio())
+
+#Atividade2:
+#print(meu_modulo.numeros_aleatorios())
+
+# #Atividade 3: 
+#print(meu_modulo.aleatorio2())
+
+#Atividade 4:
+#print (meu_modulo.contagem())
+
+
+# meu_modulo.contagem()
+
+# meu_modulo.soma_pares(6)
+
+# meu_modulo.tabuada(21)
+
+# meu_modulo.contagem2()
+
---
+++ Curso de Python - Senai/Aula_14/meu_modulo.py
@@ -0,0 +1,49 @@
+# EXERCÍCIOS:
+# Trabalhando um pouco mais com funções | loops | variáveis | listas …
+# Crie com módulos das funções utilize parâmetros/return: 
+
+# 1 - Crie um número aleatório de 5,10
+
+# def aleatorio():
+#     return random.randint(5,10)
+# aleatorio()
+
+# 2 - Crie 3 números aleatórios
+# import random 
+# def numeros_aleatorios(): 
+#     nums = []
+#     for i in range(3):
+#         nums.append(random.randint(1,8))
+#     return nums
+
+# 3 - Crie um número aleatório entre 10 a 30 utilize o range()
+# import random 
+# def aleatorio2(): 
+#     return random.randrange(10,31)
+# aleatorio2()
+
+
+#4: 
+# def contagem():
+#     for c in range(10,0,-1):
+#         print('contagem:', c)
+#     print('🔥')    
+
+#5:
+# def soma_pares(x):
+#     s = 0
+#     for i in range(2,x,2):
+#         s  =  s  + i
+#         print('numeros: ',s)
+#     print('Soma: ', s)    
+
+#6:
+# def tabuada(y):
+#     for i in range(1,11):
+#         c  =  y * i
+#         print(y,'x',i,'=',c)
+
+#7:
+# def contagem2():
+#     for x in range(99,-1,-1):
+#         print(x)   
